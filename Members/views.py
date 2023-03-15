cf579ed6681cccf410a95896717e317d8913560b
sivqnov <sbolshakov02@gmail.com>
2023-03-15 20:49:20 +0300

    redirect register login

--- Members/views.py
+++ Members/views.py
@@ -12,21 +12,24 @@ from Stock.settings import STATICFILES_DIRS
 # Create your views here.
 
 def login_user(request):
-    if request.method == "POST":
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('main')
-        else:
-            messages.success(request, ("Возникла ошибка при попытке входа в аккаунт. Попробуйте снова!"))
-            return redirect('login')
+    if request.user.is_authenticated:
+        return redirect('user', user=str(request.user))
     else:
-        context = {
-            'title': 'Вход в аккаунт',
-        }
-        return render(request, 'login.html', context)
+        if request.method == "POST":
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('main')
+            else:
+                messages.success(request, ("Возникла ошибка при попытке входа в аккаунт. Попробуйте снова!"))
+                return redirect('login')
+        else:
+            context = {
+                'title': 'Вход в аккаунт',
+            }
+            return render(request, 'login.html', context)
 
 @login_required(login_url='login')
 def logout_user(request):
@@ -36,43 +39,46 @@ def logout_user(request):
     return redirect('main')
 
 def register_user(request):
-    if request.method == "POST":
-        form = RegisterUserForm(request.POST)
-        formProfile = RegisterProfileForm(request.POST)
-        if form.is_valid() and formProfile.is_valid():
-            form.save()
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password1']
-            user = authenticate(username=username, password=password)
-            login(request, user)
-            bio = formProfile.cleaned_data['bio']
-            userobj = User.objects.get(username=username)
-            Profile.objects.create(user=userobj, photo='default_user.png', bio=bio)
-            messages.success(request, ("Регистрация успешна!"))
-            return redirect('main')
+    if request.user.is_authenticated:
+        return redirect('user', user=str(request.user))
     else:
-        form = RegisterUserForm()
-        formProfile = RegisterProfileForm()
-    context = {
-        'title': 'Регистрация',
-        'form': form,
-        'formProfile': formProfile,
-        }
-    return render(request, 'register.html', context)
+        if request.method == "POST":
+            form = RegisterUserForm(request.POST)
+            formProfile = RegisterProfileForm(request.POST)
+            if form.is_valid() and formProfile.is_valid():
+                form.save()
+                username = form.cleaned_data['username']
+                password = form.cleaned_data['password1']
+                user = authenticate(username=username, password=password)
+                login(request, user)
+                bio = formProfile.cleaned_data['bio']
+                userobj = User.objects.get(username=username)
+                Profile.objects.create(user=userobj, photo='default_user.png', bio=bio)
+                messages.success(request, ("Регистрация успешна!"))
+                return redirect('main')
+        else:
+            form = RegisterUserForm()
+            formProfile = RegisterProfileForm()
+        context = {
+            'title': 'Регистрация',
+            'form': form,
+            'formProfile': formProfile,
+            }
+        return render(request, 'register.html', context)
 
 def user_profile(request, user):
-    # try:
-    userobj = User.objects.get(username=user)
-    profile = Profile.objects.get(user = userobj.id)
-    context = {
-        'struser': str(request.user),
-        'profile': profile,
-        'request': request,
-    }
-    return render(request, 'profile.html', context)
-    # except:
-    #     messages.success(request, ("Пользователя с таким именем не найдено!"))
-    #     return redirect('main')
+    try:
+        userobj = User.objects.get(username=user)
+        profile = Profile.objects.get(user = userobj.id)
+        context = {
+            'struser': str(request.user),
+            'profile': profile,
+            'request': request,
+        }
+        return render(request, 'profile.html', context)
+    except:
+        messages.success(request, ("Пользователя с таким именем не найдено!"))
+        return redirect('main')
 
 class PasswordsChangeView(PasswordChangeView):
     form_class = PasswordChangingForm
